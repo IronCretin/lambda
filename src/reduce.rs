aa990b95bd93bdd7a950df0ed773a8e8fb9dad31
Paul Maynard <palu.maynard@gmail.com>
2019-06-19 17:10:32 -0400

    im dumb and forgot to use references

--- src/reduce.rs
+++ src/reduce.rs
@@ -24,13 +24,13 @@ pub enum Reduc {
 //     }
 // }
 
-pub fn red_basic(ex: Exp) -> Reduc {
+pub fn red_basic(ex: &Exp) -> Reduc {
     match ex {
         Call(a, b) => {
-            match *a {
+            match **a {
                 Lamb(_, _) => Reduc::Beta,
-                a => match red_basic(a) {
-                    Reduc::Irred => match red_basic(*b) {
+                _ => match red_basic(a) {
+                    Reduc::Irred => match red_basic(b) {
                         Reduc::Irred => Reduc::Irred,
                         r => Reduc::Right(Box::new(r))
                     }
@@ -42,25 +42,29 @@ pub fn red_basic(ex: Exp) -> Reduc {
     }
 }
 
-pub fn free_vars(ex: Exp) -> HashSet<String> {
+pub fn free_vars(ex: &Exp) -> HashSet<String> {
     match ex {
         Var(n) => {
             let mut v = HashSet::new();
-            v.insert(n);
+            v.insert(n.clone());
             v
         }
         Call(a, b) => {
-            let mut v = free_vars(*a);
-            v.extend(free_vars(*b));
+            let mut v = free_vars(a);
+            v.extend(free_vars(b));
             v
         }
         Lamb(x, r) => {
-            let mut v = free_vars(*r);
-            v.remove(&x);
+            let mut v = free_vars(r);
+            v.remove(x);
             v
         }
     }
 }
+
+// pub fn sub() -> Exp {
+
+// }
 
 #[cfg(test)]
 mod tests {
@@ -71,40 +75,40 @@ mod tests {
 
     #[test]
     fn irred_basic() -> Result<(), ParseError>{
-        assert_eq!(red_basic(parse("x")?), Reduc::Irred);
-        assert_eq!(red_basic(parse("a b")?), Reduc::Irred);
-        assert_eq!(red_basic(parse("\\x.x")?), Reduc::Irred);
-        assert_eq!(red_basic(parse("\\x. (\\y.y) z")?), Reduc::Irred);
+        assert_eq!(red_basic(&parse("x")?), Reduc::Irred);
+        assert_eq!(red_basic(&parse("a b")?), Reduc::Irred);
+        assert_eq!(red_basic(&parse("\\x.x")?), Reduc::Irred);
+        assert_eq!(red_basic(&parse("\\x. (\\y.y) z")?), Reduc::Irred);
         Ok(())
     }
     #[test]
     fn beta_basic() -> Result<(), ParseError> {
-        assert_eq!(red_basic(parse("(\\x. x) y")?), Reduc::Beta);
-        assert_eq!(red_basic(parse("(\\x. x) y z")?),
+        assert_eq!(red_basic(&parse("(\\x. x) y")?), Reduc::Beta);
+        assert_eq!(red_basic(&parse("(\\x. x) y z")?),
             Reduc::Left(Box::new(Reduc::Beta)));
-        assert_eq!(red_basic(parse("z ((\\x. x) y)")?),
+        assert_eq!(red_basic(&parse("z ((\\x. x) y)")?),
             Reduc::Right(Box::new(Reduc::Beta)));
         Ok(())
     }
     #[test]
     fn order_basic() -> Result<(), ParseError> {
-        assert_eq!(red_basic(parse("(\\a. a) b ((\\x. x) y)")?),
+        assert_eq!(red_basic(&parse("(\\a. a) b ((\\x. x) y)")?),
             Reduc::Left(Box::new(Reduc::Beta)));
-        assert_eq!(red_basic(parse("(\\x. (\\a. a) x) z w")?),
+        assert_eq!(red_basic(&parse("(\\x. (\\a. a) x) z w")?),
             Reduc::Left(Box::new(Reduc::Beta)));
         Ok(())
     }
     #[test]
     fn free() -> Result<(), ParseError> {
-        assert_eq!(free_vars(parse("x")?),
+        assert_eq!(free_vars(&parse("x")?),
             HashSet::from_iter(vec!["x".to_string()]));
-        assert_eq!(free_vars(parse("x y z")?),
+        assert_eq!(free_vars(&parse("x y z")?),
             HashSet::from_iter(vec!["x".to_string(), "y".to_string(), "z".to_string()]));
-        assert_eq!(free_vars(parse("(\\x. x) y")?),
+        assert_eq!(free_vars(&parse("(\\x. x) y")?),
             HashSet::from_iter(vec!["y".to_string()]));
-        assert_eq!(free_vars(parse("(\\x y. x)")?),
+        assert_eq!(free_vars(&parse("(\\x y. x)")?),
             HashSet::new());
-        assert_eq!(free_vars(parse("(\\x y. x) y")?),
+        assert_eq!(free_vars(&parse("(\\x y. x) y")?),
             HashSet::from_iter(vec!["y".to_string()]));
         Ok(())
     }
